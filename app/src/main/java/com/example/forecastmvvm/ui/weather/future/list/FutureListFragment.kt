8e9711221bd66153b2648a4df51229b542480ca7
Akshat Agrawal <akshatagrawal58@gmail.com>
2019-03-17 20:14:45 +0530

    Navigation Component added and Part1 ended

---
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/future/list/FutureListFragment.kt
@@ -0,0 +1,32 @@
+package com.example.forecastmvvm.ui.weather.future.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.forecastmvvm.R
+
+class FutureListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FutureListFragment()
+    }
+
+    private lateinit var viewModel: FutureListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.future_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(FutureListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
